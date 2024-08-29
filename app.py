943de4e902bbcaa986d29bc2659d4621686d1673
bricefotzo <brice.fotzo@hotmail.com>
2024-08-29 09:57:40 +0200

    change titel

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import streamlit as st
 API_URL = "https://data.paris2024.org/api/explore/v2.1/catalog/exports/csv?delimiter=%3B&lang=fr"
 data = pd.read_csv(API_URL, sep=";")
 data.columns = [col.replace("default.", "") for col in data.columns]
-st.title("My first App")
+st.title("Olympics Games Data")
 st.bar_chart(data, x="title", y="records_count")
